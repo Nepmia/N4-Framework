904311362c156dbe9e69365222910341776c11a3
Nepmia <64558289+Nepmia@users.noreply.github.com>
2021-06-13 19:23:56 +0200

    imported jinja and made a basic template rendering test

--- build.py
+++ build.py
@@ -1,8 +1,20 @@
-from template_registrator import templates_module, templates_exporter, template_registrator
+import settings
+# from template_registrator import templates_module, templates_exporter, template_registrator
+from jinja2 import Environment,PackageLoader, select_autoescape
+import os
 
 
+env = Environment(
+    loader=PackageLoader("app"),
+    autoescape=select_autoescape()
+)
 
-template_registrator()
+# template_registrator()
+templates_list = os.listdir(settings.APP_TEMPLATE_FOLDER)
+for template in templates_list:
+    temp = env.get_template(template)
+    print(temp.render(kek="LOL"))
+# env.render_templates(settings.APP_TEMPLATE_FOLDER,  zip=None, )
 
             
 
