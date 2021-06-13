13e3aff929465a939cf5b73a192d887c75defbea
Nepmia <64558289+Nepmia@users.noreply.github.com>
2021-06-13 11:37:47 +0200

    preparing the new html variable system

--- settings.py
+++ settings.py
@@ -1,5 +1,10 @@
+# App basic informations and folder paths 
+
 APP_NAME="app"
 APP_TEMPLATE_FOLDER=f"{APP_NAME}/templates"
 APP_STATIC_FOLDER=f"{APP_NAME}/static"
 APP_MODULE_FOLDER=f"{APP_NAME}/modules"
 APP_ROOT=f"{APP_NAME}/"
+
+
+
