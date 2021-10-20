a71bf37e4fc335823e14a6328e0392eca9bc9f15
Nepmia <yukana.miasu@gmail.com>
2021-10-21 01:34:25 +0200

    So I finished the prompting system which now works dynamically

--- helpers.py
+++ helpers.py
@@ -1,13 +1,13 @@
 from termcolor import colored
 import sys
 
-def  N4_println(message:any, important_or_dangerous:any = None, optional_message:any = None):
+def  N4_println(message:str, important_or_dangerous:str = "", optional_message:str = ""):
     """N4 CLI Printing function. Prints a message in the terminal.
 
     Args:
-        message (any):  Message to print, commonly str.
-        important_or_dangerous (any, optional): Important or dangerous information that needs to be in red for the user to notice it. Defaults to None.
-        optional_message (any, optional): Optional message after an error or an important info. Defaults to None.
+        message (str):  Message to print, commonly str.
+        important_or_dangerous (str, optional): Important or dangerous information that needs to be in red for the user to notice it. Defaults to None.
+        optional_message (str, optional): Optional message after an error or an important info. Defaults to None.
     """
     print(
         colored("[N4] ", "blue"),
@@ -16,16 +16,45 @@ def  N4_println(message:any, important_or_dangerous:any = None, optional_message
         colored(optional_message, "cyan"),
      )
 
-def N4_user_prompt(valid_answers:dict, question:str, default:str) -> str: 
-    if default == "yes":
-        possible_answer = "[Y/n]"
+def N4_user_prompt(valid_answers:dict, question:str, display_answer:str, default:str = None) -> str: 
+    """N4 CLI user interaction method. Allow N4 to ask questions to the user and alter N4 behavior.
+
+    Args:
+        valid_answers (dict): a dict of the valid answers (yes : true, no : false, y : true, n : false, maybe : maybe, idk : thing).
+        question (str): The question you ask to the user.
+        display_answer (str): The possible answers you'll show to the user (like: [Y/n]).
+        default (str, optional): Default answer that will be given if the user just press enter without entering any answer, is optional. Defaults to None.
+
+    Returns:
+        str: The value of the key of valid_answers that the user gave. 
+    """
+
+    N4_println(question)
+    N4_println("Please answer with:", display_answer)
+    while True:
+        user_answer = input().lower()
+        if user_answer is "" and default is not None:
+            return default
+        elif user_answer in valid_answers:
+            return valid_answers[user_answer]
+        else:
+            N4_println("Incorect answer.", "Please retry. \n")
+            N4_println(question)
+            N4_println("Please answer with:", display_answer)
+
+def  prompt_tester():
+    N4_println("Beginning prompt input test. Valid answers will be:", "Yes or No", "Result will be a sentence containing the answer.")
+
+    valid_answers = {
+        "yes" : True,
+        "y" : True,
+        "no" : False,
+        "n" : False,
+    }
+
+    prompt = N4_user_prompt(valid_answers, "Are you aware that this is a test?","[y/N]", "no")
+
+    if prompt is True:
+        N4_println("Yes! You knew it! Wellplayed!")
     else:
-        possible_answer = "[y/N]"
-    N4_println(question, possible_answer)
-    user_answer = input().lower()
-    if user_answer == "":
-        return valid_answers[default]
-    elif user_answer in valid_answers:
-        return valid_answers[default]
-    else:
-        N4_println("Please respond with a correct answer.")
+        N4_println("Boo... Big noob!")
--- version_writer.py
+++ version_writer.py
@@ -1,21 +1,5 @@
 from helpers import *
 
-def prompt_tester():
-    N4_println("Beginning prompt input test. Valid answers will be:", "Yes or No", "Result will be a sentence containing the answer.")
 
-    valid_answers = {
-        "yes": True,
-        "y": True,
-        "no": False,
-        "n": False,
-    }
- 
-    prompt = N4_user_prompt(valid_answers, "Are you aware that this is a test?", "no")
-    N4_println("User input is:", prompt)
+# WIP
 
-    sentence = f"You might wanna know that you've been tested! {prompt}"
-
-    N4_println("Here are your results!")
-    N4_println("", sentence)
-
-prompt_tester()
