74aae48e7dfdc024977bc568fcfa69d3a2d375fb
Nepmia <yukana.miasu@gmail.com>
2021-10-21 13:50:01 +0200

    Made a new syntax for components using html so it looks like <ComponentName arg="" arg2="" />

--- build.py
+++ build.py
@@ -1,11 +1,4 @@
 from template_handler import template_builder, templates_lister
-import app
-from template_registrator import templates_module, templates_exporter, template_registrator
-import os
 
-
-
-
-template_registrator()
 
 template_builder(templates_lister())
