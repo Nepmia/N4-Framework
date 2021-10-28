68bd5687763469fdb3366fe08cc99a8e91ee41c1
Nepmia <yukana.miasu@gmail.com>
2021-10-28 07:07:00 +0200

    still trying to fix jinja thing

--- template_handler.py
+++ template_handler.py
@@ -1,16 +1,18 @@
 from ast import Index
 from jinja2 import Environment, select_autoescape, FileSystemLoader
+from jinja2.loaders import PackageLoader
 import app
 import os
-import app.components 
+import app.components
 import re
 
 from helpers import N4_println, working_dir
 
 j_env = Environment(
-    loader=FileSystemLoader(app.COMPONENT_FOLDER),
+    loader=PackageLoader("app"),
     autoescape=select_autoescape()
 )
+
 
 def templates_lister():
     """Lists templates in app.TEMPLATE_FOLDER
@@ -21,7 +23,8 @@ def templates_lister():
     N4_println("Template list rendered.")
     return os.listdir(app.PAGES_FOLDER)
 
-def template_builder(template_list:list):
+
+def template_builder(template_list: list):
 
     for template in template_list:
 
@@ -31,8 +34,8 @@ def template_builder(template_list:list):
             N4_println("found component:", component)
             with open(os.path.join(app.PAGES_FOLDER, template), "r") as current_template:
                 openned_template = current_template.read()
-                N4_println("openned is",openned_template)
-                template_match =  re.findall(r"<(.+)\s+/>", openned_template)
+                N4_println("openned is", openned_template)
+                template_match = re.findall(r"<(.+)\s+/>", openned_template)
 
                 for item in template_match:
                     component_name = re.findall(r"^(.*?) ", item)[0]
@@ -41,22 +44,31 @@ def template_builder(template_list:list):
                     if component_name in app.components.COMPONENT_LIST:
                         N4_println("Yes!")
 
-                        parsed_arg_list = str(component_arg).replace("[","").replace("]","").replace("'","")
-                        component_path =  f"{component_name}.html"
+                        parsed_arg_list = str(component_arg).replace(
+                            "[", "").replace("]", "").replace("'", "")
+                        component_path = f"{component_name}.html"
                         parsed_component = f"{{% with {parsed_arg_list} %}}{{% include \"{component_path}\" %}}{{% endwith %}}"
-                        test = re.sub(fr"<(({component_name}).+)\s+/>", parsed_component, openned_template)
+                        test = re.sub(
+                            fr"<(({component_name}).+)\s+/>", parsed_component, openned_template)
 
                         N4_println(test)
 
-
                     else:
                         N4_println("No!")
-
 
         temp = j_env.get_template(test)
         rendered_template = temp.render()
         N4_println(rendered_template)
 
+
 def component_builder():
     for component in app.COMPONENT_FOLDER:
         pass
+
+
+def render_tester():
+    temp = j_env.get_template("{ % with name='jean', second='jean', trois='damien' % }{ % include 'testComponent.html' % }{ % endwith % }")
+    rendered_template = temp.render()
+    N4_println(rendered_template)
+
+render_tester()
